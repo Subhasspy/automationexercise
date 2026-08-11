eefa1f102c05cf4ec3a9f9a52fec2d32fe81cd36
Subhasspy <subhasspy007@gmail.com>
2026-08-11 22:52:24 +0530

    Day 1: removed the unnecessary POM file and changed / notation to . notation

--- src/test/java/com/automationexercise/runners/TestRunner.java
+++ src/test/java/com/automationexercise/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"com/automationexercise/stepdefinitions", "com/automationexercise/hooks"},
+        glue = {"com.automationexercise.stepdefinitions", "com.automationexercise.hooks"},
         plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
